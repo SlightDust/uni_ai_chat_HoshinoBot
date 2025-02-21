f68c51dd1982d90729662ecef10f63b55edfb193
SlightDust <zzc_200014@163.com>
2025-02-21 18:16:23 +0800

    摸了一个markdown转http的http接口，没测高并发，转图片不太现实

---
+++ test/markdown_render_test.py
@@ -0,0 +1,43 @@
+import os
+import uuid
+from flask import Flask, request, jsonify
+import markdown2
+
+app = Flask(__name__)
+
+_current_dir = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(_current_dir, 'md')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+@app.route('/md/render', methods=['POST'])
+def render_markdown():
+    if request.method == 'POST':
+        markdown_content = request.get_data(as_text=True)
+        if not markdown_content:
+            return jsonify({'error': 'Markdown content is required'}), 400
+
+        html_content = markdown2.markdown(markdown_content)
+        filename = str(uuid.uuid4()) + '.html'
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+        with open(filepath, 'w', encoding='utf-8') as f:
+            f.write(html_content)
+
+        url = f'/md/{filename}'
+        return jsonify({'url': url}), 200
+
+@app.route('/md/<path:filename>', methods=['GET'])
+def get_markdown_file(filename):
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    if os.path.exists(filepath):
+        with open(filepath, 'r', encoding='utf-8') as f:
+            content = f.read()
+        return content, 200
+    else:
+        return jsonify({'error': 'File not found'}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
